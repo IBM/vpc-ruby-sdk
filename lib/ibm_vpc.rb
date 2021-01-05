67c240f91efca526b39f0b16f646efe37f46ea18
Adam Grare <agrare@redhat.com>
2021-01-05 15:11:04 -0500

    Add IbmVpc::Authenticators

--- lib/ibm_vpc.rb
+++ lib/ibm_vpc.rb
@@ -5,6 +5,7 @@ module IbmVpc
   ApiException = IBMCloudSdkCore::ApiException
   DetailedResponse = IBMCloudSdkCore::DetailedResponse
 
+  require_relative "./ibm_vpc/authenticators.rb"
   require_relative "./ibm_vpc/common.rb"
   require_relative "./ibm_vpc/vpc_v1.rb"
 end
